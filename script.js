bf07c430e0e1127e69155e512bf7b84bd32dee3a
oerd bej <oerdbej@gmail.com>
2022-07-17 23:22:38 +0200

    getting users selected file

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const fileInput = document.querySelector(".file-input");
 const chooseImgBtn = document.querySelector(".choose-img");
 
+console.log(fileInput);
+
+// Getting the users selected file. Using files method from global object, if there is no file
+const loadImage = () => {
+  let file = fileInput.files[0];
+  if (!file) return;
+  console.log(file);
+};
+
+fileInput.addEventListener("change", loadImage);
+
 // Triggers the hidden input
 chooseImgBtn.addEventListener("click", () => fileInput.click());
