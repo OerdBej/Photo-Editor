89784a5d1818fc92100891d4996868455016a510
oerd bej <oerdbej@gmail.com>
2022-07-17 22:34:05 +0200

    triggering functionality for the input element

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-alert("hello");
+const fileInput = document.querySelector(".file-input");
+const chooseImgBtn = document.querySelector(".choose-img");
+
+// Triggers the hidden input
+chooseImgBtn.addEventListener("click", () => fileInput.click());
